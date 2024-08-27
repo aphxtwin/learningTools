465a9049e9ca8bbdb83adc5e42ac63eb77e7d9de
aphxtwin <ivospironello@gmail.com>
2024-08-27 10:58:46 -0300

    image filesize calculator

--- WebDev/image-compressor/image-compressor-react/src/App.tsx
+++ WebDev/image-compressor/image-compressor-react/src/App.tsx
@@ -1,9 +1,44 @@
 import './App.css';
 import { useState } from 'react';
+import imageCompression from 'browser-image-compression';
+
+const COMPRESSION_THRESHOLD = 150 * 1024;
 
 function App() {
+  const [fileSize, setFileSize] = useState<string>('')
+  // the goal of this app is the user to upload a file and then compress it
+  // after that compression the file is sent to the server for further processing
+  // the server will then return the compressed file to the user
+  const handleInputChange = async (e:React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target?.files?.[0]
+    setFileSize(String(file?.size))
+    const fileSize = Number(file?.size)
+    if(fileSize >  COMPRESSION_THRESHOLD){
+      alert(`it passes the threshold for ${fileSize - COMPRESSION_THRESHOLD} bytes`)
+      console.log(`${file?.size}`)
+    } else{
+      return
+    }
+  }
+
+  const handleUpload = async () => {
+
+    const response = await fetch('http://localhost:3001/upload', {
+      method:'POST',
+      body: ''
+    })
+    if(!response.ok){
+      alert(`HTTP error! status: ${response.status}`)
+    }
+  }
   return (
     <div className="App">
+      <div className="App">
+      <h1 className='image-comp-h1'>Image Compressor</h1>
+      <input type="file" accept="image/*" onChange={handleInputChange} />
+      <h3>fileSize</h3>
+      {fileSize  ? `${Number(fileSize)/1000} kbytes` : 'upload your file to know the size'}
+      </div>
     </div>
   );
 }
--- WebDev/image-compressor/image-upload-server/server.js
+++ WebDev/image-compressor/image-upload-server/server.js
@@ -1,6 +1,44 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const cors = require('cors');
+const multer = require('multer');
+const path = require('path');
+
+// enable cors
+app.use(cors());
+
+// set up storage
+// multer is middleware that processes file uploads -> 
+const storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+// Create uploads directory if it doesn't exist
+const fs = require('fs');
+if (!fs.existsSync('uploads')) {
+  fs.mkdirSync('uploads');
+}
+
+const upload = multer({
+    storage
+});
+
+// this is the enpoint /upload
+// why app.post can accept all these arguments
+app.post('/upload', upload.single('image'), (req, res) => {
+    if(!req.file){
+        res.status(400).send('No puta file uploaded');
+    }
+    res.send({
+        message: 'File uploaded',
+        file: req.file.filename
+    })
+}
+);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
